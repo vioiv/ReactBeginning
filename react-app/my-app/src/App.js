d5665f3d873bd28587e6b3ce8bc8ceabbecfff79
JeongHoon Kim <devkjh@gmail.com>
2019-12-20 20:46:48 +0900

    make component

--- react-app/my-app/src/App.js
+++ react-app/my-app/src/App.js
@@ -1,44 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import { render } from 'react-dom';
 
-function Subject() {
-  return (
-    <header>
-        <h1>WEB</h1>
-        World wide web!
-    </header>
-  );
+class Subject extends Component {
+  render(){
+    return (
+      <header>
+          <h1>{this.props.title}</h1>
+          {this.props.sub}
+      </header>
+    );
+  }
 }
 
-function App() {
-  return (
-    <div className="App">
-      <Subject></Subject>
-      <TOC></TOC>
-      <Content>ß</Content>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <Subject title="WEB" sub="world wide web!"></Subject>
+        <Subject title="React" sub="For UI"></Subject>
+        <TOC></TOC>
+        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+      </div>
+    );
+  }
 }
 
-function TOC(){
-  return (
-    <nav>
-        <ul>
-            <li><a href="1.html">HTML</a></li>
-            <li><a href="2.html">CSS</a></li>
-            <li><a href="3.html">JavaScript</a></li>
-        </ul>
-    </nav>
-  );
+class TOC extends Component{
+  render(){
+    return (
+      <nav>
+          <ul>
+              <li><a href="1.html">HTML</a></li>
+              <li><a href="2.html">CSS</a></li>
+              <li><a href="3.html">JavaScript</a></li>
+          </ul>
+      </nav>
+    );
+  }
 }
 
-function Content(){
-  return(
-    <article>
-    <h2>HTML</h2>
-    HTML is HyperText Markup Language.
-    </article>
-  );
+class Content extends Component{
+  render(){
+    return(
+      <article>
+      <h2>{this.props.title}</h2>
+      {this.props.desc}
+      </article>
+    );
+  }
 }
 export default App;
